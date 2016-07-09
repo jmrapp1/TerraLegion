58ef788d865960a867848a7a0f0c1773aee7a954
sgwerder <gwerdersimon@hotmail.com>
2016-07-10 01:03:08 +0200

    Added animated player

--- core/src/com/jmrapp/terralegion/game/world/entity/impl/Player.java
+++ core/src/com/jmrapp/terralegion/game/world/entity/impl/Player.java
@@ -2,8 +2,9 @@ package com.jmrapp.terralegion.game.world.entity.impl;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
+import com.jmrapp.terralegion.engine.views.drawables.AnimationDrawable;
 import com.jmrapp.terralegion.engine.views.drawables.ResourceManager;
-import com.jmrapp.terralegion.engine.views.drawables.TexturedDrawable;
+import com.jmrapp.terralegion.engine.views.drawables.SpriteSheet;
 import com.jmrapp.terralegion.engine.world.entity.BodyType;
 import com.jmrapp.terralegion.game.item.inventory.Inventory;
 import com.jmrapp.terralegion.game.world.chunk.Chunk;
@@ -15,9 +16,13 @@ public class Player extends LivingEntity {
 	private Inventory inventory;
 
 	public Player(float x, float y) {
-		super(new TexturedDrawable(ResourceManager.getInstance().getTexture("player")), x, y, BodyType.DYNAMIC, 12f, 100, 100, 6.5f);
+		super(createAnimationDrawable(), x, y, BodyType.DYNAMIC, 12f, 100, 100, 6.5f);
 		inventory = new Inventory(5, 8);
 	}
+
+    private static AnimationDrawable createAnimationDrawable() {
+        return new AnimationDrawable(new SpriteSheet(ResourceManager.getInstance().getTexture("playerAnimated"), 32, 36, 4, 4));
+    }
 
 	public void pickUpDrop(Drop drop, Chunk chunk) {
 		if (inventory.addItemStack(drop.getItem(), drop.getStackCount())) {
@@ -26,6 +31,23 @@ public class Player extends LivingEntity {
 		}
 	}
 
+    /**
+     * Positive values means going to the east, negative values going to the west.
+     * @param velocity
+     */
+    public void addVelocity(float velocity) {
+        addVelocity(velocity, 0);
+        AnimationDrawable animationDrawable = (AnimationDrawable) drawable;
+        if(velocity < 0) {
+            animationDrawable.setAnimationByType(AnimationDrawable.Type.WALK_WEST);
+            animationDrawable.update();
+        }
+        else if(velocity > 0) {
+            animationDrawable.setAnimationByType(AnimationDrawable.Type.WALK_EAST);
+            animationDrawable.update();
+        }
+    }
+
 	@Override
 	public void update() {
 		if (Gdx.input.isKeyPressed(Keys.SPACE) || Gdx.input.isKeyPressed(Keys.W) || Gdx.input.isKeyPressed(Keys.UP)) {
@@ -33,10 +55,10 @@ public class Player extends LivingEntity {
 				jump();
 		}
 		if (Gdx.input.isKeyPressed(Keys.A) || Gdx.input.isKeyPressed(Keys.LEFT)) {
-			addVelocity(-12f, 0);
+			addVelocity(-12f);
 		}
 		if (Gdx.input.isKeyPressed(Keys.D) || Gdx.input.isKeyPressed(Keys.RIGHT)) {
-			addVelocity(12f, 0);
+			addVelocity(12f);
 		}
 	}
 
