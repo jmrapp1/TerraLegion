ffea5b12071b887f2f2c3ae5eabcb1242091bf9b
Jordan <jhouse112233@gmail.com>
2016-08-04 11:21:01 -0500

    Fix chest using taken ID

--- core/src/com/jmrapp/terralegion/game/world/block/BlockType.java
+++ core/src/com/jmrapp/terralegion/game/world/block/BlockType.java
@@ -3,7 +3,7 @@ package com.jmrapp.terralegion.game.world.block;
 public enum BlockType {
 
 	AIR(0), DIRT(1), GRASS(2), STONE(3), DIAMOND(4), TORCH(5), STONE_WALL(6), COAL(8), DIRT_WALL(9),
-    WOOD(10), LEAVES(11), WOOD_CHEST(12);
+    WOOD(10), LEAVES(11), WOOD_CHEST(14);
 
     private static final BlockType[] blockTypeArray = values();
 	private final int id;
